5312779a19e15a5829da151823a5dc989aaca2f8
Manji Kerai <admin@thecodecentre.co.uk>
2016-02-07 11:25:05 +0000

    updated oauth;

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var did;
 function onSignIn(googleUser) {
 // Useful data for your client-side scripts:
 	profile = googleUser.getBasicProfile();
-//	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-//	console.log("Name: " + profile.getName());
-//	console.log("Image URL: " + profile.getImageUrl());
+	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	console.log("Name: " + profile.getName());
+	console.log("Image URL: " + profile.getImageUrl());
 	console.log("Email: " + profile.getEmail());
 
 	// The ID token you need to pass to your backend:
